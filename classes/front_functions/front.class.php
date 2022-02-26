e2208d52e61a009ec5d9bfc6195eac62f6cc095d
Osman Onat <84340387+OsmanOnat@users.noreply.github.com>
2022-02-26 03:20:05 +0300

    Update front.class.php

--- classes/front_functions/front.class.php
+++ classes/front_functions/front.class.php
@@ -31,8 +31,8 @@ class FrontFunctions{
      */
 
     public static function br(int $counter = 1){
-        if($counter == 0){
-            echo __FUNCTION__ .'() fonksiyonundaki counter değeri 0 girilemez';
+        if($counter <= 0){
+            echo __FUNCTION__ .'() fonksiyonundaki counter değeri 0 ve negatif girilemez';
         }else{
             for($i = 0; $i < $counter; $i++){
                 echo '
